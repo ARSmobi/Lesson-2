c3b7ec6c1b9026f36c683870a45c713aed105568
ARSmobi <102361458+ARSmobi@users.noreply.github.com>
2022-03-26 22:07:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Задание_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 75;
+            double d = 1.55;
+            float f = 12.8f;
+            bool b = true;
+            decimal c = 128.256m;
+            char e = 'G';
+            var vA = 32;
+            var vB = 552.67;
+            var vC = 91.3456m;
+            string s = "ЯЮниор";
+            byte bb = 255;
+            sbyte sbb = 0;
+            short sh = -25789;
+            ushort ush = 58768;
+            uint ui = 1758938765;
+            long l = -5758968765475869;
+            ulong ul = 14765834133226764567;
+        }
+    }
+}
